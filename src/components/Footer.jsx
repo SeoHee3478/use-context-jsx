06c7bef43749fa0bb76a7c2a3f1ea138870c2d14
SeoHee3478 <seohee3478@gmail.com>
2023-11-09 16:03:27 +0900

    refac: zotai 상태관리 라이브러리 활용하여 theme 기능 리팩토링

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useAtom } from "jotai";
+import { themeAtom } from "../jotai/atom";
 
-export default function Footer({ isDark, toggleThema }) {
+export default function Footer() {
+  const [theme, setTheme] = useAtom(themeAtom);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
   return (
-    <footer className={`footer ${isDark ? "dark" : "white"}`}>
-      <button onClick={toggleThema}>테마 변경</button>
+    <footer className={`footer ${theme === "light" ? "white" : "dark"}`}>
+      <button onClick={toggleTheme}>테마 변경</button>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useAtom } from "jotai";
+import { themeAtom } from "../jotai/atom";
 
-export default function Header({ isDark }) {
+export default function Header() {
+  const [theme] = useAtom(themeAtom);
   return (
-    <header className={`header ${isDark ? "dark" : "white"}`}>
+    <header className={`header ${theme === "light" ? "light" : "dark"}`}>
       <h1>Welcome! 프론트엔드 테킷 수강생</h1>
     </header>
   );
