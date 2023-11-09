59eee92987fcc5cb65d49bf7596ef54709152a57
SeoHee3478 <seohee3478@gmail.com>
2023-11-09 14:09:36 +0900

    feat: theme store 생성

---
+++ src/components/theme.js
@@ -0,0 +1,10 @@
+import create from "zustand";
+
+export const useThemeStore = create((set) => ({
+  theme: "light",
+  setTheme: (newTheme) => {
+    set(() => ({
+      theme: newTheme,
+    }));
+  },
+}));
