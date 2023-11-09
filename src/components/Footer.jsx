701c0624c713cb1a7d7ddc0e2ab72c6d1a863917
SeoHee3478 <seohee3478@gmail.com>
2023-11-09 14:10:10 +0900

    feat: theme 적용 및 toggle 기능 구현

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useThemeStore } from "./theme";
 
-export default function Footer({ isDark, toggleThema }) {
+export default function Footer() {
+  const { theme, setTheme } = useThemeStore();
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
   return (
-    <footer className={`footer ${isDark ? "dark" : "white"}`}>
-      <button onClick={toggleThema}>테마 변경</button>
+    <footer className={`footer ${theme === "light" ? "white" : "dark"}`}>
+      <button onClick={toggleTheme}>테마 변경</button>
     </footer>
   );
 }
